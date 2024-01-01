1e0bb4737fc6adce8b8114e8066e0882651d2166
Yonatan Eisenberg <yonatan.eis@gmail.com>
2024-01-01 11:38:49 +0200

    added aws creds to env and settings

--- earthquake_data_layer/settings.py
+++ earthquake_data_layer/settings.py
@@ -24,6 +24,12 @@ DEBUG = False
 API_HOST = os.getenv("API_HOST", None)
 API_KEYs = get_api_keys()
 
+# aws
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID", None)
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY", None)
+AWS_REGION = os.getenv("AWS_REGION", None)
+AWS_BUCKET_NAME = os.getenv("AWS_BUCKET_NAME", None)
+
 # metadata
 LOCAL_METADATA = True
 
